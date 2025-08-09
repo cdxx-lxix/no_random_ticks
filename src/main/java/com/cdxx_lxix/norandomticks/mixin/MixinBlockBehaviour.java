467e39c751421b2d47a57a0a2fce60315f818661
CDXX-LXIX <alex@mladich.com>
2025-08-09 20:21:14 +0200

    Removed unused imports

--- src/main/java/com/cdxx_lxix/norandomticks/mixin/MixinBlockBehaviour.java
+++ src/main/java/com/cdxx_lxix/norandomticks/mixin/MixinBlockBehaviour.java
@@ -5,15 +5,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets="net.minecraft.world.level.block.state.BlockBehaviour$BlockStateBase")
 public abstract class MixinBlockBehaviour {
