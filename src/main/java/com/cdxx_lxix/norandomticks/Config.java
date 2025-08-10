a05c631a1b861529a3a524736f7b824078c7d8d4
CDXX-LXIX <alex@mladich.com>
2025-08-10 16:02:38 +0200

    New debug config option

--- src/main/java/com/cdxx_lxix/norandomticks/Config.java
+++ src/main/java/com/cdxx_lxix/norandomticks/Config.java
@@ -11,15 +11,36 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
+    private enum Properties {
+        DEBUG  ("debug"),
+        RANDOM ("randomticks_blocks"),
+        TICKS  ("ticks_blocks");
+
+        private final String key;
+
+        Properties(String key) {
+            this.key = key;
+        }
+
+        public String key() {
+            return key;
+        }
+    }
+
     // List of block resource locations to blacklist random ticks
     public static final ModConfigSpec.ConfigValue<List<? extends String>> BLOCK_STRINGS_RANDOM = BUILDER
             .comment("A list of blocks (resource IDs) whose random ticking will be prevented. Example: [\"minecraft:copper_block\", \"minecraft:bamboo\"]")
-            .defineListAllowEmpty("randomticks_blocks", List.of(), () -> "", Config::validateBlockName);
+            .defineListAllowEmpty(Properties.RANDOM.key(), List.of(), () -> "", Config::validateBlockName);
 
     // List of block resource locations to blacklist ticking
     public static final ModConfigSpec.ConfigValue<List<? extends String>> BLOCK_STRINGS_TICKS = BUILDER
             .comment("A list of blocks (resource IDs) whose ticking will be prevented. USE WITH CAUTION! Example: [\"minecraft:fire\"]")
-            .defineListAllowEmpty("ticks_blocks", List.of(), () -> "", Config::validateBlockName);
+            .defineListAllowEmpty(Properties.TICKS.key(), List.of(), () -> "", Config::validateBlockName);
+
+    // Debug option
+    public static final ModConfigSpec.ConfigValue<Boolean> DEBUG = BUILDER
+            .comment("Option to receive comments of the mod's actions. On load\reload it'll print blacklisted items and items failed to pass validation")
+            .define(Properties.DEBUG.key(), true);
 
     static final ModConfigSpec SPEC = BUILDER.build();
 
@@ -27,9 +48,16 @@ public class Config {
     public static final ObjectOpenHashSet<String> BLACKLIST_RANDOM = new ObjectOpenHashSet<>();
     public static final ObjectOpenHashSet<String> BLACKLIST_TICKS = new ObjectOpenHashSet<>();
 
+    public static final ObjectOpenHashSet<String> FAILED_BLACKLIST = new ObjectOpenHashSet<>();
+
     // Validation function to ensure each string is a registered block resource location
     private static boolean validateBlockName(final Object obj) {
-        return obj instanceof String name && BuiltInRegistries.BLOCK.containsKey(ResourceLocation.parse(name));
+        if (obj instanceof String name && BuiltInRegistries.BLOCK.containsKey(ResourceLocation.parse(name))) {
+            return true;
+        } else {
+            FAILED_BLACKLIST.add(obj.toString());
+            return false;
+        }
     }
 
     public static void onConfigLoading(final ModConfigEvent.Loading event) {
@@ -52,7 +80,11 @@ public class Config {
         BLACKLIST_RANDOM.addAll(BLOCK_STRINGS_RANDOM.get());
         BLACKLIST_TICKS.addAll(BLOCK_STRINGS_TICKS.get());
 
-        NoRandomTicks.LOGGER.debug("Random Ticks Blacklist: {}", BLACKLIST_RANDOM);
-        NoRandomTicks.LOGGER.debug("Ticks Blacklist: {}", BLACKLIST_TICKS);
+        if(DEBUG.get()) {
+            NoRandomTicks.LOGGER.info("NORANDOMTICK DEBUG is ON. If you don't want this messages navigate to the config and change 'true' to 'false'");
+            NoRandomTicks.LOGGER.info("Random Ticks Blacklist: {}", BLACKLIST_RANDOM);
+            NoRandomTicks.LOGGER.info("Ticks Blacklist: {}", BLACKLIST_TICKS);
+            NoRandomTicks.LOGGER.warn("Failed to validate: {}", FAILED_BLACKLIST);
+        }
     }
 }
